7bcfa0d68ecd9a3ce62e590cdba764759d49eb29
maxmay94 <maxmay94@gmail.com>
2022-12-31 16:53:47 -0500

    add transition delay and hover color change

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="flex bg-pink-600 h-screen">
-      <div className='bg-orange-600/90 h-3/4 w-3/4 m-auto align-middle  rounded-full flex'>
-        <div className='bg-blue-600/90 h-3/4 w-3/4 m-auto align-middle  rounded-full flex'>
-
+    <div className="flex transition-colors bg-pink-600 hover:bg-sky-700 duration-700 h-screen">
+      <div className='transition-colors bg-orange-600  hover:bg-pink-600 duration-2000 h-2/3 w-2/3 m-auto align-middle shadow-2xl rounded-full flex'>
+        <div className='transition-colors bg-blue-600 hover:bg-emerald-600 h-3/4 duration-1000 w-3/4 m-auto align-middle rounded-full flex'>
+          <div className='transition-colors bg-lime-600 hover:bg-red-500 h-3/4 duration-1000 w-3/4 m-auto align-middle rounded-full flex'>
+            <div className='transition-colors bg-red-100 hover:bg-slate-800 h-3/4 duration-1000 w-3/4 m-auto align-middle rounded-full flex'>
+            </div>
+          </div>
         </div>
       </div>
     </div>
