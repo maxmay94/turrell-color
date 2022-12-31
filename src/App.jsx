c041a9628f7f01f384e76579a3a35014d106d14e
maxmay94 <maxmay94@gmail.com>
2022-12-31 17:10:52 -0500

    add last colors

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="flex transition-colors bg-pink-600 hover:bg-sky-700 duration-700 h-screen">
-      <div className='transition-colors bg-orange-600  hover:bg-pink-600 duration-2000 h-2/3 w-2/3 m-auto align-middle shadow-2xl rounded-full flex'>
-        <div className='transition-colors bg-blue-600 hover:bg-emerald-600 h-3/4 duration-1000 w-3/4 m-auto align-middle rounded-full flex'>
-          <div className='transition-colors bg-lime-600 hover:bg-red-500 h-3/4 duration-1000 w-3/4 m-auto align-middle rounded-full flex'>
-            <div className='transition-colors bg-red-100 hover:bg-slate-800 h-3/4 duration-1000 w-3/4 m-auto align-middle rounded-full flex'>
+    <div className="flex transition-colors bg-pink-600 hover:bg-sky-900 duration-700 h-screen hov:pt-40">
+      <div className='transition-colors bg-orange-600  hover:bg-yellow-600 duration-1000 h-2/3 w-2/3 m-auto align-middle shadow-2xl rounded-full flex hov:animate-bounce'>
+        <div className='transition-colors bg-blue-600 hover:bg-emerald-600 h-3/4 duration-1000 w-3/4 m-auto align-middle rounded-full flex hov:animate-bounce'>
+          <div className='transition-colors bg-lime-600 hover:bg-red-500 h-3/4 duration-1000 w-3/4 m-auto align-middle rounded-full flex hov:animate-bounce'>
+            <div className='transition-colors bg-red-100 hover:bg-slate-800 h-3/4 duration-1000 w-3/4 m-auto align-middle rounded-full flex hov:animate-bounce'>
+              <div className='transition-colors bg-red-900 hover:bg-teal-200 h-3/4 duration-1000 w-3/4 m-auto align-middle rounded-full flex hov:animate-bounce'>
+                <div className='transition-colors bg-yellow-400 hover:bg-slate-400 h-3/4 duration-1000 w-3/4 m-auto align-middle rounded-full flex hov:animate-bounce'>
+                </div>
+              </div>
             </div>
           </div>
         </div>
