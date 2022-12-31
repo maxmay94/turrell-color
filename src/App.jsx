0ce6aae1401841243871c72efd02f4295f2f8090
maxmay94 <maxmay94@gmail.com>
2022-12-31 16:38:54 -0500

    adding some colors

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex bg-pink-600 h-screen">
+      <div className='bg-orange-600/90 h-3/4 w-3/4 m-auto align-middle  rounded-full flex'>
+        <div className='bg-blue-600/90 h-3/4 w-3/4 m-auto align-middle  rounded-full flex'>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
